a39e1570073f21fa1acf89ad9061461ccede6fa9
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-10 08:52:15 -0400

    completed section 5.9

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
+#left off on 5.9
+
+  def index
+   @articles = Article.all
+  end
+
+  def show
+     @article = Article.find(params[:id])
+   end
 
   def new
   end
